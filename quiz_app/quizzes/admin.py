693e1bf46f3f5d01b62d286983c28b64fc7cab95
Joshua Russell <jkrussell756@gmail.com>
2018-09-11 12:02:10 -0400

    Basic admin configuration, added views for the API routes

--- quiz_app/quizzes/admin.py
+++ quiz_app/quizzes/admin.py
@@ -1,3 +1,24 @@
+# Import modules
 from django.contrib import admin
+from .models import Quiz, Question, Answer
 
-# Register your models here.
+class AnswerInLine(admin.TabularInline):
+    model = Answer
+    extra = 5
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Question', {'fields': ['text']})
+    ]
+    inlines = [AnswerInLine]
+
+class QuizAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Quiz Name', {'fields': ['name']})
+    ]
+    inlines = [QuestionAdmin]
+    list_display = ('name', 'pub_date')
+    list_filter = ['pub_date']
+    search_fields = ['name', 'series']
+
+admin.site.register(Quiz, Question, QuizAdmin, QuestionAdmin)
--- quiz_app/quizzes/views.py
+++ quiz_app/quizzes/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+# Import Modules
+from quizzes.models import Quiz, Question, Answer
+from quizzes.serializers import QuizSerializer, QuestionSerializer, AnswerSerializer
+from django.views import generic
 
-# Create your views here.
+# API Views.
+
+# View all the quizzes
+class QuizList(generic.ListView):
+    queryset = Quiz.objects.all()
+    serializer_class = QuizSerializer
+
+# View a single quiz
+class SingleQuiz(generic.DetailView):
+    model = Quiz
+    serializer_class = QuizSerializer
