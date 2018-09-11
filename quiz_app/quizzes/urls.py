91ee2d80864acaf59f5a23aa318b1eaa6b6a8e56
Joshua Russell <jkrussell756@gmail.com>
2018-09-11 12:25:12 -0400

    Views for the API should run now, added the quizzes urls to the main project, got rid of uneccesary importations

--- quiz_app/quizzes/urls.py
+++ quiz_app/quizzes/urls.py
@@ -1,8 +1,6 @@
 # Import Modules
 from django.urls import path, re_path
 from django.contrib import admin
-from django.conf import settings
-from django.conf.urls.static import static
 from . import views
 
 # API/Admin URLs
